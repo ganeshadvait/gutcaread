8f7fc80cbba6321d054a8c2571a2368a40aa0999
Ganesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-02-23 15:13:37 +0530

    fonts added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 import Header from "./components/header/header";
 import Footer from "./components/Footer/footer";
 
